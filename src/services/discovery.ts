88235e2de63a1a179a07ba3f6cf95f63d7294889
Lance Ball <lball@redhat.com>
2020-07-03 13:25:18 -0400

    fixup: fix linting and type issues
    Signed-off-by: Lance Ball <lball@redhat.com>

--- src/services/discovery.ts
+++ src/services/discovery.ts
@@ -1,4 +1,16 @@
-import { CloudEventV1Service, CloudEventV1Type } from "../event";
+import { CloudEventV1Service } from "../event";
+
+interface RequestHandler {
+  (
+    req: { query: { matching: string }; url: string },
+    res: {
+      end: () => void;
+      status: (
+        code: number,
+      ) => { json: (obj: Record<string, unknown> | CloudEventV1Service[]) => string; end: () => void };
+    },
+  ): void;
+}
 
 /**
  * DiscoveryService to implement the discovery spec:
@@ -24,15 +36,17 @@ export class DiscoveryService {
    *
    * TODO Implement after discussion with team
    * It would add a @Service in front of class to declare a new service
+   * @returns {void}
    */
-  static Service() {
+  static Service(): void {
     // TO IMPLEMENT
   }
 
   /**
    * Register your service to the DiscoveryService
    *
-   * @param service to register
+   * @param {CloudEventV1Service} service to register
+   * @returns {void}
    */
   static registerService(service: CloudEventV1Service): void {
     if (DiscoveryService.servicesMap[service.name]) {
@@ -47,6 +61,7 @@ export class DiscoveryService {
 
   /**
    * Retrieve all services
+   * @returns {CloudEventV1Service[]} all discovery services
    */
   static getServices(): CloudEventV1Service[] {
     return this.searchService();
@@ -54,9 +69,10 @@ export class DiscoveryService {
 
   /**
    * Search for a service
-   * @param term to search for, case insensitive
+   * @param {string} term to search for, case insensitive
+   * @returns {CloudEventV1Service[]} all discovery services matching `term`
    */
-  static searchService(term: string = ""): CloudEventV1Service[] {
+  static searchService(term = ""): CloudEventV1Service[] {
     //
     const searchTerm = term.toLowerCase();
     return Object.keys(DiscoveryService.servicesMap)
@@ -66,6 +82,7 @@ export class DiscoveryService {
 
   /**
    * Retrieve all event types
+   * @returns {CloudEventV1Service[]} all discovery services
    */
   static getTypes(): { [key: string]: CloudEventV1Service[] } {
     return this.searchType();
@@ -73,11 +90,12 @@ export class DiscoveryService {
 
   /**
    * Search for a type of event
-   * @param term to search for, case insensitive
+   * @param {string} term to search for, case insensitive
+   * @returns {CloudEventV1Service[]} all discovery services matching `type`
    */
-  static searchType(term: string = ""): { [key: string]: CloudEventV1Service[] } {
+  static searchType(term = ""): { [key: string]: CloudEventV1Service[] } {
     const searchTerm = term.toLowerCase();
-    let result: { [key: string]: CloudEventV1Service[] } = {};
+    const result: { [key: string]: CloudEventV1Service[] } = {};
     Object.keys(DiscoveryService.eventsMap)
       .filter((k) => term === "" || k.toLowerCase().includes(searchTerm))
       .forEach((k) => (result[k] = DiscoveryService.eventsMap[k]));
@@ -87,17 +105,18 @@ export class DiscoveryService {
   /**
    * Express handler
    *
-   * You can add it
+   * You can add it to your express application
    *
-   * @param app Your express app
-   * @param prefix Prefix for all discovery url
-   * @param permissions Callback to implement CloudEvent permissions
+   * @param {Function} app Your express app
+   * @param {string} prefix Prefix for all discovery url
+   * @param {Function} permissions Callback to implement CloudEvent permissions
+   * @returns {void}
    */
   static express(
-    app: any,
-    prefix: string = "",
-    permissions: (name: string, type: "Service" | "Type", req: any) => boolean = () => true,
-  ) {
+    app: { get: (path: string | RegExp, handler: RequestHandler) => void },
+    prefix = "",
+    permissions: (name: string, type: "Service" | "Type", req: unknown) => boolean = () => true,
+  ): void {
     /**
      * Based on the spec
      * Note: for each query if the client is not authorized to see any particular
@@ -106,13 +125,17 @@ export class DiscoveryService {
      * Therefore we filter events from service as they are an entity
      *
      * TODO Need to confirm this with the group
+     * @param {CloudEventV1Service} object a service object
+     * @param {Record<string, unknown>} req a request object
+     * @returns {CloudEventV1Service} services
      */
-    const filterTypes = (object: CloudEventV1Service, req: any) => {
-      return { ...object, types: object.types.filter((type) => permissions(type.type, "Type", req)) };
-    };
+    const filterTypes = (object: CloudEventV1Service, req: Record<string, unknown>) => ({
+      ...object,
+      types: object.types.filter((type) => permissions(type.type, "Type", req)),
+    });
     // Implement services listing
-    app.get(`${prefix}/services`, (req: any, res: any) => {
-      let term = req.query.matching || "";
+    app.get(`${prefix}/services`, (req, res) => {
+      const term = req.query.matching || "";
       res.status(200).json(
         DiscoveryService.searchService(term)
           .filter((service) => permissions(service.name, "Service", req))
@@ -120,8 +143,8 @@ export class DiscoveryService {
       );
       res.end();
     });
-    app.get(new RegExp(prefix + "/services/.+"), (req: any, res: any) => {
-      let name = req.url.substr(prefix.length + "/services/".length);
+    app.get(new RegExp(`${prefix}/services/.+`), (req, res) => {
+      const name = req.url.substr(prefix.length + "/services/".length);
       if (
         // Non existing service
         !DiscoveryService.servicesMap[name] ||
@@ -133,11 +156,11 @@ export class DiscoveryService {
       }
       res.status(200).json(filterTypes(DiscoveryService.servicesMap[name], req));
     });
-    app.get(`${prefix}/types`, (req: any, res: any) => {
-      let term = req.query.matching || "";
-      let types = DiscoveryService.searchType(term);
-      let result: { [key: string]: CloudEventV1Service[] } = {};
-      for (let i in types) {
+    app.get(`${prefix}/types`, (req, res) => {
+      const term = req.query.matching || "";
+      const types = DiscoveryService.searchType(term);
+      const result: { [key: string]: CloudEventV1Service[] } = {};
+      for (const i in types) {
         if (permissions(i, "Type", req)) {
           result[i] = types[i]
             .filter((service) => permissions(service.name, "Service", req))
@@ -147,8 +170,8 @@ export class DiscoveryService {
       res.status(200).json(result);
       res.end();
     });
-    app.get(new RegExp(prefix + "/types/.+"), (req: any, res: any) => {
-      let name = req.url.substr(prefix.length + "/types/".length);
+    app.get(new RegExp(`${prefix}/types/.+`), (req, res) => {
+      const name = req.url.substr(prefix.length + "/types/".length);
       if (
         // Non existing type
         !DiscoveryService.eventsMap[name] ||
@@ -159,7 +182,7 @@ export class DiscoveryService {
         return;
       }
       // Filter service and events from service
-      let result = DiscoveryService.eventsMap[name]
+      const result = DiscoveryService.eventsMap[name]
         .filter((service) => permissions(service.name, "Service", req))
         .map((service) => filterTypes(service, req));
       // If no service with this event is available to the user return 404
